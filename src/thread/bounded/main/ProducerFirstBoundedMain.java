e88ddfc466900f48dab63f0827708bc118489f60
minsukim9900 <mmsskksw9900@gmail.com>
2026-01-03 14:58:35 +0900

    section9: 생산자 소비자 문제1

---
+++ src/thread/bounded/main/ProducerFirstBoundedMain.java
@@ -0,0 +1,59 @@
+package thread.bounded.main;
+
+import thread.bounded.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ProducerFirstBoundedMain {
+    public static void main(String[] args) {
+        // 1BoundedQueue 선택
+        BoundedQueue queue = new BoundedQueueV3(2);
+
+        producerFirst(queue);
+    }
+
+    private static void producerFirst(BoundedQueue queue) {
+        log("== [생산자 먼저 실행] 시작, " + queue.getClass().getSimpleName() + " ==");
+        List<Thread> threads = new ArrayList<>();
+        startProducer(queue, threads);
+        printAllState(queue, threads);
+        startConsumer(queue, threads);
+        printAllState(queue, threads);
+        log("== [생산자 먼저 실행] 종료, " + queue.getClass().getSimpleName() + " ==");
+    }
+
+
+    private static void startProducer(BoundedQueue queue, List<Thread> threads) {
+        log("생산자 시작");
+
+        for (int i = 1; i <= 3; i++) {
+            Thread producer = new Thread(new ProducerTask(queue, "data" + i), "producer" + i);
+            threads.add(producer);
+            producer.start();
+            sleep(100);
+        }
+    }
+
+    private static void startConsumer(BoundedQueue queue, List<Thread> threads) {
+        log("소비자 시작");
+
+        for (int i = 1; i <= 3; i++) {
+            Thread consumer = new Thread(new ConsumerTask(queue), "consumer" + i);
+            threads.add(consumer);
+            consumer.start();
+            sleep(100);
+        }
+    }
+
+    private static void printAllState(BoundedQueue queue, List<Thread> threads) {
+        log("현재 상태 출력, 큐 데이터: " + queue);
+
+        for (Thread thread : threads) {
+            log(thread.getName() + ": " + thread.getState());
+        }
+    }
+}
