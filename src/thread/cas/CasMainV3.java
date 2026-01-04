c610924010c68de5e971ca29a26025eee2d64f0b
minsukim9900 <mmsskksw9900@gmail.com>
2026-01-04 17:37:06 +0900

    section11: CAS - 동기화와 원자적 연산

---
+++ src/thread/cas/CasMainV3.java
@@ -0,0 +1,53 @@
+package thread.cas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class CasMainV3 {
+    
+    private static final int THREAD_COUNT = 100;
+
+    public static void main(String[] args) throws InterruptedException {
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        System.out.println("start value = " + atomicInteger.get());
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                increamentAndGet(atomicInteger);
+            }
+        };
+
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            Thread thread = new Thread(runnable);
+            threads.add(thread);
+            thread.start();
+        }
+        
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        
+        int result = atomicInteger.get();
+        System.out.println(atomicInteger.getClass().getSimpleName() + " value = " + result);
+    }
+
+    private static int increamentAndGet(AtomicInteger atomicInteger) {
+        int getValue = 0;
+        boolean result;
+
+        do {
+            getValue = atomicInteger.get();
+            log("getValue = " + getValue);
+            result = atomicInteger.compareAndSet(getValue, getValue + 1);
+            log("result: " + result);
+        } while(!result);
+
+        return getValue + 1;
+    }
+}
